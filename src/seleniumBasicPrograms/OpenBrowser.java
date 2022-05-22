2fdb5b72e538cb9cce0a326d24bba05f76f5f236
ushaAutomate <usha.sriramoju@gmail.com>
2022-05-22 21:40:02 +0530

    New File has been added

---
+++ src/seleniumBasicPrograms/OpenBrowser.java
@@ -0,0 +1,17 @@
+package seleniumBasicPrograms;
+
+public class OpenBrowser {
+ static 	WebDriver driver;
+	public static void main(String[] args) {
+
+	
+		System.setProperty("webdriveer.chrome.driver", "\\Users\\Lucky\\eclipse-workspace\\SeleniumPractice\\src\\drivers\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		
+		//driver.get("http://wwww.webmd.com");
+		//driver.close();
+		
+		
+	}
+
+}
